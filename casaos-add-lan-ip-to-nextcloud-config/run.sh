8c3ef07b36491763a2b8d1191ed146877c21f229
ricardogfp <50072612+ricardogfp@users.noreply.github.com>
2024-11-10 20:35:18 +0100

    Update run.sh

--- casaos-add-lan-ip-to-nextcloud-config/run.sh
+++ casaos-add-lan-ip-to-nextcloud-config/run.sh
@@ -4,13 +4,13 @@
 lan_ip=$(hostname -I | awk '{print $1}')
 
 # Backup the original config.php file
-cp /DATA/AppData/big-bear-nextcloud/html/config/config.php /DATA/AppData/big-bear-nextcloud/html/config/config.php.bak
+cp /DATA/AppData/big-bear-nextcloud-smb/html/config/config.php /DATA/AppData/big-bear-nextcloud-smb/html/config/config.php.bak
 
 # Add the LAN IP to the config.php file
-awk -v ip="$lan_ip" '/0 => '\''localhost'\''/{print; print "    1 => '\''"ip"'\'',"; next}1' /DATA/AppData/big-bear-nextcloud/html/config/config.php.bak > /DATA/AppData/big-bear-nextcloud/html/config/config.php
+awk -v ip="$lan_ip" '/0 => '\''localhost'\''/{print; print "    1 => '\''"ip"'\'',"; next}1' /DATA/AppData/big-bear-nextcloud-smb/html/config/config.php.bak > /DATA/AppData/big-bear-nextcloud-smb/html/config/config.php
 
 # Get the path to the docker-compose.yml file
-COMPOSE_FILE="/var/lib/casaos/apps/big-bear-nextcloud/docker-compose.yml"
+COMPOSE_FILE="/var/lib/casaos/apps/big-bear-nextcloud-smb/docker-compose.yml"
 
 # Apply changes using casaos-cli
 casaos-cli app-management apply "big-bear-nextcloud" --file="$COMPOSE_FILE"
